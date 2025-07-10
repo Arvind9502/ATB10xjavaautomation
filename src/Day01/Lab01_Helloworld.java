af9e20e26bf4fef6bc269eac83bbb0c05d48c93a
Arvind <arvindjadhav53@gmail.com>
2025-07-10 20:19:04 +0530

    commit

---
+++ src/Day01/Lab01_Helloworld.java
@@ -0,0 +1,8 @@
+package Day01;
+
+public class Lab01_Helloworld {
+    public static void main (String[] args)
+    {
+        System.out.println("first program");
+    }
+}
---
+++ src/Day01/Lab02_BasicArithmeticOperations.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class Lab02_BasicArithmeticOperations {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
